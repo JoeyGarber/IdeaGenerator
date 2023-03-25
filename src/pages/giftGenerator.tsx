7b52e47814450d53380fbc31fdff4cc7e836f668
joeygarber <joseph.z.garber@gmail.com>
2023-03-24 23:38:37 -0400

    Built form, got scroll working

--- src/pages/giftGenerator.tsx
+++ src/pages/giftGenerator.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import { generateIdeas } from '../api/openAi'
 
 export default function GiftGenerator () {
+  const [page, setPage] = useState(0)
   const [budget, setBudget] = useState('')
   const [age, setAge] = useState('')
-  const [gender, setGender] = useState('')
+  const [gender, setGender] = useState('male')
   const [interest, setInterest] = useState('')
   const [giftIdeas, setGiftIdeas] = useState('')
 
@@ -38,14 +39,76 @@ export default function GiftGenerator () {
     )
   }
 
+  const allParamsFilled = () => {
+    return budget && age && gender && interest
+  }
+
+  const conditionalForm = () => {
+    switch (page) {
+      case 0:
+        return  <div className="flex flex-col items-center">
+                  <h3 className="text-xl font-bold">Budget</h3>
+                  <input autoFocus onFocus={(e) => e.currentTarget.select()} className="outline outline-1 m-1" type="text" pattern="\d*"  placeholder="budget" value={budget} onChange={(e) => setBudget(e.target.value)}  />
+                </div>
+      case 1:
+        return  <div className="flex flex-col items-center">
+                  <h3 className="text-xl font-bold">Age</h3>
+                  <input autoFocus onFocus={(e) => e.currentTarget.select()} className="outline outline-1 m-1" type="text" pattern="\d*"  placeholder="age" value={age} onChange={(e) => setAge(e.target.value)}  />
+                </div>
+      case 2:
+        return  <div>
+                  <h3 className="text-xl font-bold">Gender</h3>
+                  <select className="p-1" name="gender" id="gender" value={gender} onChange={(e) => setGender(e.target.value)}>
+                    <option value="select option" disabled={true}>Select Option</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </select>
+                </div>
+      case 3:
+        return  <div className="flex flex-col items-center">
+                  <h3 className="text-xl font-bold">Interest</h3>
+                  <input autoFocus onFocus={(e) => e.currentTarget.select()} className="outline outline-1 m-1" type="text" placeholder="interest" value={interest} onChange={(e) => setInterest(e.target.value)}  />
+                </div>
+      case 4:
+        if (allParamsFilled()) {
+          return <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1" onClick={() => generateGifts(budget, age, gender, interest)}>Generate Ideas!</button>
+        }
+        return <p>You missed a field!</p>
+    }
+  }
+
+  const handleNext = () => {
+    setPage((prev) => prev + 1)
+  }
+
+  const handlePrev = () => {
+    if (page > 0) {
+      setPage((prev) => prev - 1)
+    }
+  }
+
   return (
     <div className="flex flex-col items-center">
-      <p className="text-3xl font-bold underline m-1">Hi! How are you?</p>
-      <input className="outline outline-1 m-1" type="text" placeholder="budget" value={budget} onChange={(e) => setBudget(e.target.value)} />
-      <input className="outline outline-1 m-1" type="text" placeholder="age" value={age} onChange={(e) => setAge(e.target.value)} />
-      <input className="outline outline-1 m-1" type="text" placeholder="gender" value={gender} onChange={(e) => setGender(e.target.value)} />
-      <input className="outline outline-1 m-1" type="text" placeholder="interest" value={interest} onChange={(e) => setInterest(e.target.value)} />
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1" onClick={() => generateGifts(budget, age, gender, interest)}>Click me!</button>
+      <h1 className="text-3xl font-bold underline m-1">Gift Generator</h1>
+      {conditionalForm()}
+        <div>
+          {page > 0 &&
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1" onClick={handlePrev}>
+              Back
+            </button>
+          }
+          {page < 4 &&
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1" onClick={handleNext}>
+            Next
+            </button>
+          }
+        <div>
+          <p>Budget: {budget ? 'Filled' : 'Need to fill'}</p>
+          <p>Age: {age ? 'Filled' : 'Need to fill'}</p>
+          <p>Gender: {gender ? 'Filled' : 'Need to fill'}</p>
+          <p>Interest: {interest ? 'Filled' : 'Need to fill'}</p>
+        </div>
+        </div>
       {giftIdeas && formatOutput(giftIdeas)}
     </div>
   )
