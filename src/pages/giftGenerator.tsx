f0ee86b5210091284f08b21e445b2ec410722588
joeygarber <joseph.z.garber@gmail.com>
2023-03-27 23:03:54 -0400

    Added images, gave them functionality

--- src/pages/giftGenerator.tsx
+++ src/pages/giftGenerator.tsx
@@ -1,18 +1,25 @@
 import { useState } from 'react'
 import { generateIdeas } from '../api/openAi'
 
+import Female from '../assets/Female.png'
+import Male from '../assets/Male.png'
+import UpToTwenty from '../assets/UpToTwenty.png'
+import UpToForty from '../assets/UpToForty.png'
+import UpToHundred from '../assets/UpToHundred.png'
+import HundredOrMore from '../assets/HundredOrMore.png'
+
 export default function GiftGenerator () {
   const [page, setPage] = useState(0)
   const [budget, setBudget] = useState('')
   const [age, setAge] = useState('')
-  const [gender, setGender] = useState('male')
+  const [gender, setGender] = useState('')
   const [interest, setInterest] = useState('')
   const [giftIdeas, setGiftIdeas] = useState('')
 
   const generateGifts = (budget: string, age: string, gender: string, interest: string) => {
     const model = "gpt-3.5-turbo"
     if (budget && age && gender && interest) {
-      const content = `Give me a list of gift ideas without descriptions for gifts under ${budget} dollars for a(n) ${age} ${gender} who is interested in ${interest}`
+      const content = `Give me a list of gift ideas without descriptions for gifts ${budget} for a(n) ${age} ${gender} who is interested in ${interest}`
       generateIdeas({ model, content })
       .then((resp: any) => {
         console.log(resp.data.choices[0].message.content)
@@ -47,26 +54,44 @@ export default function GiftGenerator () {
     switch (page) {
       case 0:
         return  <div className="flex flex-col items-center">
-                  <h3 className="text-xl font-bold">Budget</h3>
-                  <input autoFocus onFocus={(e) => e.currentTarget.select()} className="outline outline-1 m-1" type="text" pattern="\d*"  placeholder="budget" value={budget} onChange={(e) => setBudget(e.target.value)}  />
+                  <h3 className="text-xl font-bold">Select Your Recipient's Gender</h3>
+                  <img alt='woman icon' className="w-[12vw] border-2 border-pink-500 rounded-lg m-1" src={Female} onClick={() => {
+                    setGender('female')
+                    setPage((prev) => prev + 1)
+                  }} />
+                  <img alt='man icon' className="w-[12vw] border-2 border-blue-500 rounded-lg m-1" src={Male} onClick={() => {
+                    setGender('male')
+                    setPage((prev) => prev + 1)
+                  }} />
                 </div>
       case 1:
         return  <div className="flex flex-col items-center">
-                  <h3 className="text-xl font-bold">Age</h3>
-                  <input autoFocus onFocus={(e) => e.currentTarget.select()} className="outline outline-1 m-1" type="text" pattern="\d*"  placeholder="age" value={age} onChange={(e) => setAge(e.target.value)}  />
+                  <h3 className="text-xl font-bold">Select Your Budget</h3>
+                  <img alt='Up To Twenty' className="w-[8vw] border-2 border-green-500 rounded-lg" src={UpToTwenty} onClick={() => {
+                    setBudget('up to 20 dollars')
+                    setPage((prev) => prev + 1)
+                  }} />
+                  <img alt='Up to Forty' className="w-[8vw] border-2 border-green-500 rounded-lg" src={UpToForty} onClick={() => {
+                    setBudget('up to 40 dollars')
+                    setPage((prev) => prev + 1)
+                  }} />
+                  <img alt='Up to One Hundred' className="w-[8vw] border-2 border-green-500 rounded-lg" src={UpToHundred} onClick={() => {
+                    setBudget('up to 100 dollars')
+                    setPage((prev) => prev + 1)
+                  }} />
+                  <img alt='One Hundred or More' className="w-[8vw] border-2 border-green-500 rounded-lg" src={HundredOrMore} onClick={() => {
+                    setBudget('one hundred dollars or more')
+                    setPage((prev) => prev + 1)
+                  }} />
                 </div>
       case 2:
         return  <div className="flex flex-col items-center">
-                  <h3 className="text-xl font-bold">Gender</h3>
-                  <select className="p-1" name="gender" id="gender" value={gender} onChange={(e) => setGender(e.target.value)}>
-                    <option value="select option" disabled={true}>Select Option</option>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                  </select>
+                  <h3 className="text-xl font-bold">Select Your Recipient's Age</h3>
+                  <input autoFocus onFocus={(e) => e.currentTarget.select()} className="outline outline-1 m-1" type="text" pattern="\d*"  placeholder="age" value={age} onChange={(e) => setAge(e.target.value)}  />
                 </div>
       case 3:
         return  <div className="flex flex-col items-center">
-                  <h3 className="text-xl font-bold">Interest</h3>
+                  <h3 className="text-xl font-bold">Input Your Recipient's Interests</h3>
                   <input autoFocus onFocus={(e) => e.currentTarget.select()} className="outline outline-1 m-1" type="text" placeholder="interest" value={interest} onChange={(e) => setInterest(e.target.value)}  />
                 </div>
       case 4:
@@ -97,17 +122,11 @@ export default function GiftGenerator () {
               Back
             </button>
           }
-          {page < 4 &&
+          {page > 1 && page < 4 &&
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1" onClick={handleNext}>
             Next
             </button>
           }
-        <div>
-          <p>Budget: {budget ? 'Filled' : 'Need to fill'}</p>
-          <p>Age: {age ? 'Filled' : 'Need to fill'}</p>
-          <p>Gender: {gender ? 'Filled' : 'Need to fill'}</p>
-          <p>Interest: {interest ? 'Filled' : 'Need to fill'}</p>
-        </div>
         </div>
       {giftIdeas && formatOutput(giftIdeas)}
     </div>
